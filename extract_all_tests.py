079c7cd8c036c331176ed68e4ed53f9f1dc7559e
Muhammad Sufiyan Sadiq <118098066+Sufiyan2003@users.noreply.github.com>
2026-04-06 21:17:57 +0500

    add yamls for new testcases

--- extract_all_tests.py
+++ extract_all_tests.py
@@ -1,18 +1,22 @@
 import yaml
+import sys
 
-file_name = "ci_test_spec.yaml"
 
-def parse_yaml();
+# get this yaml file and extract all the test cases from this here yaml
+file_name = "./verif/ci_test_spec.yaml"
 
-	with open(file_name, 'r') as f:
-		data = yaml.load(f, Loader=yaml.SafeLoader)
-		
-	print data
-	
-	return 0
+def parse_yaml(test_name):
+    with open(file_name, 'r') as f:
+        data = yaml.safe_load(f)
 
+    if test_name not in data:
+        print(f"Error: {test_name} not found in YAML", file=sys.stderr)
+        sys.exit(1)
+
+    plusargs = data[test_name].get("plusargs", [])
+    return " ".join(plusargs)
 
 if __name__ == "__main__":
-	print("Beginning parsing test spec yaml")
-	parse_yaml()
+    test_name = sys.argv[1]
+    print(parse_yaml(test_name))
 
